dcc96609ffe36ff1aa72f95a300262e4b0b2e876
hejtmy <hejtmy@gmail.com>
2019-05-15 02:34:14 +0200

    Changes the structure a bit

---
+++ demo/TestingConnection.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using BrainVR.Eyetracking.PupilLabs;
+
+public class TestingConnection : MonoBehaviour
+{
+    private PupilManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = PupilManager.Instance;
+        manager.Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
